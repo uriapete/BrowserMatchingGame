d2d9fb43368c16f29feeb10a8bf208ad3fac587f
uriapete <uriarteangelo@gmail.com>
2023-02-27 13:10:52 -0800

    Initialized index.html, css, js, and started ts project

---
+++ js/gameScript.js
@@ -0,0 +1,4 @@
+"use strict";
+const helloWorldMsg = "Hello World!";
+console.log(helloWorldMsg);
+// do comments travel over too?
---
+++ js/gameScript.ts
@@ -0,0 +1,8 @@
+const helloWorldMsg : string = "Hello World!";
+console.log(helloWorldMsg);
+
+// do comments travel over too? indeed they do.
+
+// this is my first time playing with typescript so putting this here to remind myself:
+
+// remember to tsc -p jsFolder or else all of your lets and consts will be turned into disgusting vars

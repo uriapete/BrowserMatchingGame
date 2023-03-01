ef433fc9ff4b86d1f43e479a882a976a6bbaec27
uriapete <uriarteangelo@gmail.com>
2023-02-28 16:55:15 -0800

    Begun DOM interfacing in TS/JS + edited congrats and gamemode nodes in HTML, and h2 CSS

--- js/gameScript.js
+++ js/gameScript.js
@@ -1,4 +1,34 @@
 "use strict";
-const helloWorldMsg = "Hello World!";
-console.log(helloWorldMsg);
-// do comments travel over too?
+// const helloWorldMsg : string = "Hello World!";
+// console.log(helloWorldMsg);
+// do comments travel over too? indeed they do.
+// this is my first time playing with typescript so putting this here to remind myself:
+// remember to tsc -p jsFolder or else all of your lets and consts will be turned into disgusting vars
+// getting gameboard
+const gameBoard = document.querySelector(".gameBoard");
+// console.log(gameBoard);
+// getting setup area
+const gameSetUp = document.querySelector("div.gameSetUp");
+// console.log(gameSetUp);
+// getting row input
+const rowInput = gameSetUp.querySelector("input#rows-input");
+// console.log(rowInput);
+// getting columns input
+const columnsInput = gameSetUp.querySelector("input#columns-input");
+// console.log(columnsInput);
+// getting start button
+const startBtn = gameSetUp.querySelector("button#startButton");
+// console.log(startBtn);
+// making a test event listener for the rows, columns, and start button
+// startBtn.addEventListener("click",()=>{
+//     console.log(rowInput.value)
+//     console.log(columnsInput.value)
+// })
+// all good
+// getting gamemode h2
+const gameMode = document.querySelector("h2.gameMode");
+// console.log(gameMode);
+const congrats = document.querySelector("h2.congrats");
+// console.log(congrats);
+const numOfCards = document.querySelector("span#numOfCards");
+console.log(numOfCards);
--- js/gameScript.ts
+++ js/gameScript.ts
@@ -1,8 +1,56 @@
-const helloWorldMsg : string = "Hello World!";
-console.log(helloWorldMsg);
+// const helloWorldMsg : string = "Hello World!";
+// console.log(helloWorldMsg);
 
 // do comments travel over too? indeed they do.
 
 // this is my first time playing with typescript so putting this here to remind myself:
 
 // remember to tsc -p jsFolder or else all of your lets and consts will be turned into disgusting vars
+
+// getting gameboard
+const gameBoard : HTMLDivElement = document.querySelector(".gameBoard")!;
+
+// console.log(gameBoard);
+
+// getting setup area
+const gameSetUp : HTMLDivElement = document.querySelector("div.gameSetUp")!;
+
+// console.log(gameSetUp);
+
+// getting row input
+const rowInput : HTMLInputElement = gameSetUp.querySelector("input#rows-input")!;
+
+// console.log(rowInput);
+
+// getting columns input
+const columnsInput : HTMLInputElement = gameSetUp.querySelector("input#columns-input")!;
+
+// console.log(columnsInput);
+
+// getting start button
+const startBtn : HTMLDivElement = gameSetUp.querySelector("button#startButton")!;
+
+// console.log(startBtn);
+
+// making a test event listener for the rows, columns, and start button
+// startBtn.addEventListener("click",()=>{
+//     console.log(rowInput.value)
+//     console.log(columnsInput.value)
+// })
+// all good
+
+// getting gamemode h2
+const gameMode : HTMLHeadingElement = document.querySelector("h2.gameMode")!;
+
+// console.log(gameMode);
+
+const congrats : HTMLHeadingElement = document.querySelector("h2.congrats")!;
+
+// console.log(congrats);
+
+const numOfCards : HTMLSpanElement = document.querySelector("span#numOfCards")!;
+
+// console.log(numOfCards);
+// i think it's all good but if not I'll put more
+
+// now for the JS-side game vars
